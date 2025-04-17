ca59dd3b7c366b36040c8786bb87fb28379ddf24
roman <romaxid@yandex.by>
2025-04-17 22:09:16 +0300

    рефакторинг

--- ai.py
+++ ai.py
@@ -1,51 +1,9 @@
-# from graph import graph
-# from state import user_states, NegotiationState
-#
-#
-# async def run_graph(message_text: str, user_id: int):
-#     print('Запустили граф')
-#     if user_id not in user_states:
-#         user_states[user_id] = NegotiationState()
-#     else:
-#         user_state = user_states[user_id]
-#         user_state.user_message = message_text
-#         user_state.history.append(f"🧑: {message_text}")
-#
-#     # Запускаем граф
-#     result: NegotiationState = graph.invoke(user_states[user_id])
-#
-#     # Сохраняем новое состояние (для следующего хода)
-#     # user_states[user_id].update(result)
-#
-#     # Если результат — соглашение или отказ, формируем сообщение для клиента
-#
-#     # user_states.pop(user_id)  # очищаем после завершения
-#
-#     if result.decision == 'accept':
-#         return f"""
-#                 🎉 Переговоры завершены с успехом!
-#                 - Решение: Принято
-#                 - Формат: {result.offer_type.value}
-#                 - Стоимость: {result.current_offer}
-#         """.strip()
-#
-#     if result.decision == 'reject':
-#         return f"""
-#                 ❌ Переговоры завершены с отказом.
-#                 - Решение: {result['decision']}
-#                 - Причина: {result['reason']}
-#         """.strip()
-#
-#     # Если переговоры продолжаются — возвращаем сообщение от бота
-#     return result.bot_message
-import copy
-
 from graph import graph
 from state import NegotiationState
 
 def run_graph(state: NegotiationState) -> tuple[str, NegotiationState]:
-    """Упрощённая версия без дублирования логики"""
-    new_state: NegotiationState = graph.invoke(state)
+    """Запуск графа и проверка его состояния после исполнения"""
+    new_state = graph.invoke(state)
 
     if new_state['decision'] == 'accept':
         response = f"🎉 Успех!\nФормат: {new_state['offer_type'].value}\nЦена: {new_state['current_offer']}"
