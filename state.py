8aff5d700437a23e91162313d3fb33513970041a
roman <romaxid@yandex.by>
2025-04-17 22:10:49 +0300

    изменено состояние + рефакторинг

--- state.py
+++ state.py
@@ -1,9 +1,6 @@
 from enum import Enum
 from typing import Optional, TypedDict
 
-from pydantic import BaseModel, Field
-
-
 class OfferType(Enum):
     fixed = "Фиксированная"
     gap = 'С гэпом'
@@ -11,29 +8,6 @@ class OfferType(Enum):
 class Decision(Enum):
     accept = 'accept'
     reject = 'reject'
-
-
-# class NegotiationState(BaseModel):
-#     cpm: float = Field(default=0)
-#     min_views: int = Field(default=0)
-#     max_views: int = Field(default=0)
-#
-#     user_message: Optional[str] = Field(default='')
-#     bot_message: Optional[str] = Field(default='')
-#     propose_count: int = Field(default=1)
-#
-#     decision: str = Field(default='negotiation')
-#     history: list[str] = Field(default=[])
-#
-#     current_offer: Optional[float] = Field(default=0)
-#
-#     finalize_message: Optional[str] = Field(default=None)
-#     offer_type: Optional[OfferType] = Field(default=OfferType.fixed)
-#
-#     reason: Optional[str] = Field(default='')
-#
-#     awaiting_user_response: bool = Field(default=False)
-
 
 class NegotiationState(TypedDict):
     cpm: float
@@ -53,10 +27,6 @@ class NegotiationState(TypedDict):
     offer_type: Optional[OfferType]
 
     reason: Optional[str]
-
-    awaiting_user_response: bool
-
-    tg_id: int
 
 
 user_states: dict[int, NegotiationState] = {}
