0451617a0b3af36759b6326735e612a28e73c893
roman <romaxid@yandex.by>
2025-04-24 10:53:32 +0300

    рефакторинг + добавление функционала возобновления работы графа

--- ai.py
+++ ai.py
@@ -1,20 +1,19 @@
-import uuid
+from langgraph.types import Command
 
 from graph import graph
 from state import NegotiationState
 
 
-async def run_graph(state: NegotiationState) -> tuple[str, NegotiationState]:
+async def run_graph(state: NegotiationState, user_config, resume=None) -> tuple[str, NegotiationState]:
     """Запуск графа и проверка его состояния после исполнения"""
-    config = {
-        "configurable": {
-            "thread_id": uuid.uuid4(),
-        }
-    }
-    new_state = await graph.ainvoke(state, config)
+    new_state: NegotiationState
+    if resume:
+        new_state = await graph.ainvoke(Command(resume=state['user_message']), config=user_config)
+    else:
+        new_state = await graph.ainvoke(state, config=user_config)
 
     if new_state['decision'] == 'accept':
-        response = f"🎉 Успех!\nФормат: {new_state['offer_type'].value}\nЦена: {new_state['current_offer']}"
+        response = f"🎉 Успех!\nФормат: {new_state['offer_type']}\nЦена: {new_state['current_offer']}"
     elif new_state['decision'] == 'reject':
         response = f"❌ Отказ: {new_state['reason']}"
     else:
