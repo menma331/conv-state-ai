f9197aea9bc21d8e2487f32fd48d9f23f12fd025
roman <romaxid@yandex.by>
2025-04-24 10:54:50 +0300

    рефакторинг

--- nodes/finalize_node.py
+++ nodes/finalize_node.py
@@ -1,11 +1,14 @@
+from langgraph.types import Command
+
 from state import NegotiationState
 from langgraph.constants import END
 
 
 def finalize_node(state: NegotiationState):
+    """Финальный узел. Формирует сообщение для пользователя и заканчивает работу графа."""
     if state['decision'] == "accept":
-        state['finalize_message'] = f"Сделка заключена! Ставка: {state['current_offer']}"
+        state['finalize_message'] = f"Сделка заключена! Ставка:\n {state['current_offer']}\nФормат: {state['offer_type']}"
     else:
         state['finalize_message'] = f"Переговоры провалились. Причина: {state['reason']}"
 
-    return {"next_node": END, "state": state}
+    return Command(goto=END, update=state)
