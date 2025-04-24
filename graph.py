335f3903b3ba9a0b7547fc8e007fe39c056a611b
roman <romaxid@yandex.by>
2025-04-24 10:54:34 +0300

    изменение узлов

--- graph.py
+++ graph.py
@@ -16,31 +16,16 @@ builder = StateGraph(NegotiationState)
 # Узлы
 builder.add_node("start", start_node)
 builder.add_node('desired_rate', desired_rate_node)
+builder.add_node('user_input', user_propose_node)
 builder.add_node("negotiation", negotiate_node)
-builder.add_node('user_propose_node', user_propose_node)
 builder.add_node("finalize", finalize_node)
 
 # Переходы
 builder.set_entry_point("start")
 builder.add_edge('start', 'desired_rate')
-builder.add_edge('desired_rate', 'negotiation')
-builder.add_edge('negotiation', 'user_propose_node')
-builder.add_edge('negotiation', 'finalize')
+builder.add_edge('desired_rate', 'user_input')
 
-builder.add_conditional_edges(
-    "desired_rate",
-    lambda state: "negotiation" if state.decision == "negotiation" else "finalize"
-)
-builder.add_conditional_edges(
-    "negotiation",
-    lambda state: "finalize" if state.decision == "accept" else "negotiation"
-)
 builder.add_edge("finalize", END)
 
-config = {
-
-    "thread_id": uuid.uuid4(),
-
-}
 checkpointer = MemorySaver()
 graph = builder.compile(checkpointer=checkpointer)
